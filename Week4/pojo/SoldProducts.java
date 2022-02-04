8adc174af77089f7999a6cae4fcf730b5aa3ef7e
Beks01 <79849100+Beks01@users.noreply.github.com>
2022-02-04 20:04:24 +0600

    Add files via upload

---
+++ Week4/pojo/SoldProducts.java
@@ -0,0 +1,37 @@
+package practiceforneobis;
+
+public class SoldProducts {
+    private int id;
+    private String date;
+    private int workerID;
+
+    public SoldProducts(int id, String date, int workerID) {
+        this.id = id;
+        this.date = date;
+        this.workerID = workerID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getWorkerID() {
+        return workerID;
+    }
+
+    public void setWorkerID(int workerID) {
+        this.workerID = workerID;
+    }
+}
