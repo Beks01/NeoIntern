0737ba6ccf0c8d3514b9417f6dfcd762995c0ac7
Beksultan Avazov <79849100+Beks01@users.noreply.github.com>
2022-06-24 02:10:44 +0600

    Delete week7/amazing directory

--- week7/amazing/src/main/java/com/example/amazing/Product/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.amazing.Product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductsRepository extends CrudRepository<Products,Integer> {
-
-
-}
--- week7/amazing/src/main/resources/db.migration/V1__InitTables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE customer
-(
-    id   BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    age  INT  NOT NULL,
-    UNIQUE (name)
-
-);
